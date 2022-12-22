35599a3cc34230a23ec4ee1e64c911b4d94b6b96
HUDOdev <huepper.dominik@gmail.com>
2022-12-22 15:14:59 +0100

    Finish exercise 7

--- ex7/ex7/modules.py
+++ ex7/ex7/modules.py
@@ -6,6 +6,12 @@ import torch.nn as nn
 # it calculates the derivatives in x and y direction
 edge_detector_kernel = torch.zeros(2, 1, 2, 2)
 
+edge_detector_kernel[0,0,:,:] = torch.tensor([[-1,0],
+                                                [1,0]])
+        
+edge_detector_kernel[1,0,:,:] = torch.tensor([[-1,1],
+                                                [0,0]])
+
 
 class Conv2d(nn.Module):
     
@@ -22,7 +28,20 @@ class Conv2d(nn.Module):
         # using self.stride as stride
         # The output is expected to be of size C x H' x W'
 
+        C, H, W = x.shape
+        C_out, C_in, h, w = self.kernel.shape
         
+        H_out = (H-h) // self.stride + 1
+        W_out = (W-w) // self.stride + 1
+
+        x_out = torch.zeros(C_out,H_out,W_out)
+
+        for i in range(H_out):
+            for j in range(W_out):
+                patch = x[:,i*self.stride:i*self.stride+h,j*self.stride:j*self.stride+w] * self.kernel
+                x_out[:,i,j] = patch.sum(dim=([1,2,3]))
+        
+        return x_out
 
         pass
 
